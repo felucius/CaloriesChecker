956005fad32d5acc6fcc82f1b955376ca3ee8ccc
Maxime <maximedelange1991@hotmail.com>
2016-12-15 16:38:46 +0100

    Added category material design assets and cleared whitespaces of every screen

--- MyApplication/app/src/main/java/maximedelange/calorieschecker/Screens/CategoryScreen.java
+++ MyApplication/app/src/main/java/maximedelange/calorieschecker/Screens/CategoryScreen.java
@@ -1,6 +1,8 @@
 package maximedelange.calorieschecker.Screens;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -8,17 +10,22 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-import maximedelange.calorieschecker.Controllers.ProductController;
-import maximedelange.calorieschecker.Domain.Product;
 import maximedelange.calorieschecker.R;
 
 public class CategoryScreen extends AppCompatActivity {
+
+    // Fields
+    private Bitmap bitmap;
+    private Bitmap resizedbitmap;
 
     // GUI Components
     private TextView lblBreakfast;
     private TextView lblLunch;
     private TextView lblDinner;
     private ImageButton btnCategory;
+    private ImageButton breakfastImage;
+    private ImageButton lunchImage;
+    private ImageButton dinnerImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +40,17 @@ public class CategoryScreen extends AppCompatActivity {
     }
 
     public void goToBreakfast(){
+        breakfastImage = (ImageButton)findViewById(R.id.btnBreakfast);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.breakfast);
+        int width=500;
+        int height=500;
+        resizedbitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
+        breakfastImage.setImageBitmap(resizedbitmap);
+
         lblBreakfast = (TextView)findViewById(R.id.lblBreakfastShow);
         lblBreakfast.setText("Breakfast");
-        lblBreakfast.setTextSize(24);
+        lblBreakfast.setTextSize(30);
         btnCategory = (ImageButton)findViewById(R.id.btnBreakfast);
         btnCategory.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,9 +62,17 @@ public class CategoryScreen extends AppCompatActivity {
     }
 
     public void goToLunch(){
+        lunchImage = (ImageButton)findViewById(R.id.btnLunch);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.lunch);
+        int width=500;
+        int height=500;
+        resizedbitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
+        lunchImage.setImageBitmap(resizedbitmap);
+
         lblLunch = (TextView)findViewById(R.id.lblLunchShow);
         lblLunch.setText("Lunch");
-        lblLunch.setTextSize(24);
+        lblLunch.setTextSize(30);
         btnCategory = (ImageButton)findViewById(R.id.btnLunch);
         btnCategory.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -61,9 +84,17 @@ public class CategoryScreen extends AppCompatActivity {
     }
 
     public void goToDinner(){
+        dinnerImage = (ImageButton)findViewById(R.id.btnDinner);
+
+        bitmap = BitmapFactory.decodeResource(getResources(), R.mipmap.dinner);
+        int width=500;
+        int height=500;
+        resizedbitmap = Bitmap.createScaledBitmap(bitmap, width, height, true);
+        dinnerImage.setImageBitmap(resizedbitmap);
+
         lblDinner = (TextView)findViewById(R.id.lblDinnerShow);
         lblDinner.setText("Dinner");
-        lblDinner.setTextSize(24);
+        lblDinner.setTextSize(30);
         btnCategory = (ImageButton)findViewById(R.id.btnDinner);
         btnCategory.setOnClickListener(new View.OnClickListener() {
             @Override
